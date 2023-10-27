22f3a0250110b948fcea4dd18f2f5eda9216b708
hafizalrifqi09 <hafizalrifqi@smkwikrama.sch.id>
2023-10-27 14:54:12 +0700

    apaajadeh

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASE_URL', 'http://localhost/php-mvc/studykasus/public/');
+
+// db
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'db_pinjam');
